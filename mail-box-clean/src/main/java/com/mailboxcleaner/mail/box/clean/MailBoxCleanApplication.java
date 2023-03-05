6044a843ec5633aa0873c4ea2b8356b48c118be4
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-03-05 21:06:19 +0530

    clean not working

---
+++ mail-box-clean/src/main/java/com/mailboxcleaner/mail/box/clean/MailBoxCleanApplication.java
@@ -0,0 +1,13 @@
+package com.mailboxcleaner.mail.box.clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailBoxCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailBoxCleanApplication.class, args);
+	}
+
+}
---
+++ mail-box-clean/src/test/java/com/mailboxcleaner/mail/box/clean/MailBoxCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailboxcleaner.mail.box.clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailBoxCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
